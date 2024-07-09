a0361675691db2368b211033a3cd2ccb175645ee
Ana Caroline Cagliari Cappellari <carolinecagliari@gmail.com>
2024-07-09 11:37:40 -0300

    3. Loops e tentativas

--- aula/app.js
+++ aula/app.js
@@ -3,10 +3,19 @@ alert("Boas vindas ao Jogo do Número Secreto");
 let numeroSecreto = 5
 console.log(numeroSecreto);
 
-let numeroChute = prompt("Escolha um número entre 1 e 10: ");
+let numeroChute;
+let tentativas = 1;
 
-if (numeroChute == numeroSecreto) {
-    alert(`Você descobriu o número secreto = ${numeroSecreto}`);
-} else {
-    alert("Você errou.");
+while (numeroChute != numeroSecreto) {
+    numeroChute = prompt("Escolha um número entre 1 e 10: ");
+    if (numeroChute == numeroSecreto) {
+        alert(`Você descobriu o número secreto = ${numeroSecreto} com ${tentativas} tentativa(s).`);
+    } else {
+        if (numeroChute < numeroSecreto) {
+            alert(`O número secreto é maior que ${numeroChute}`);
+        } else {
+            alert(`O número secreto é menor que ${numeroChute}`);
+        }
+        tentativas++;
+    }
 }
---
+++ desafio/desafio3.js
@@ -0,0 +1,33 @@
+//desafio 1
+let contadorIncremento = 1;
+
+while (contadorIncremento <= 10) {
+    alert(`Contador incremento: ${contadorIncremento}`);
+    contadorIncremento++;
+}
+
+//desafio 2
+let contadorDecremento = 10;
+
+while (contadorDecremento >= 1) {
+    alert(`Contador decremento: ${contadorDecremento}`);
+    contadorDecremento--;
+}
+
+//desafio 3
+let contagemRegressiva = prompt("Digite um número para a contagem regressiva: ");
+
+while (contagemRegressiva > 0) {
+    alert(`Contador regressiva: ${contagemRegressiva}`);7
+    contagemRegressiva--;
+}
+alert("Feliz Ano Novo! :D");
+
+//desafio 4
+let contagemProgressiva = prompt("Digite um número para a contagem progressiva: ");
+let contador = 1;
+
+while (contador <= contagemProgressiva) {
+    alert(`Contagem progressiva: ${contador}`);
+    contador++;
+}
