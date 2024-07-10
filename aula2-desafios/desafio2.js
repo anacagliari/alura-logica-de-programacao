890acc4c79ef0088d43c2483fd014d8045593f35
Ana Caroline Cagliari Cappellari <carolinecagliari@gmail.com>
2024-07-10 11:33:03 -0300

    2. Funções

---
+++ aula2-desafios/desafio2.js
@@ -0,0 +1,35 @@
+//desafio 1
+function exibirOlaMundo() {
+    console.log("Olá, mundo!");
+}
+exibirOlaMundo();
+
+//desafio 2
+function recebeNome(nome){
+    console.log(`Olá, ${nome}!`);
+}
+recebeNome("Ana Caroline");
+
+//desafio 3
+function dobrarNumero(numero){
+    return numero *2;
+}
+console.log(dobrarNumero(3));
+
+//desafio 4
+function mediaDeTres(valor1, valor2, valor3) {
+    return (valor1 + valor2 + valor3) / 3;
+}
+console.log(mediaDeTres(1, 2, 3));
+
+//desafio 5
+function maiorNumero(valor1, valor2) {
+    return valor1 > valor2 ? valor1 : valor2;
+}
+console.log(maiorNumero(12, 36));
+
+//desafio 6
+function multiplicarNumero(numero) {
+    return numero * numero;
+}
+console.log(multiplicarNumero(8));
--- aula2=explore-funcoes-listas/app.js
+++ aula2=explore-funcoes-listas/app.js
@@ -1,9 +1,19 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+let numeroAleatorio = gerarNumeroAleatorio();
+let numeroMaximo = 10;
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', `Escolha um número entre 1 e ${numeroMaximo}`);
 
 function verificarChute() {
-    console.log('O botão foi clicado.');
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroAleatorio);
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
 }
